07edc76ddf4819640eaf1ae2e0b71ab42a512491
Chris Wahlfeldt <chrisw@pixotech.com>
2022-08-29 17:59:12 -0500

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="bg-zinc-900">
+  <div class="container mx-auto px-10 h-screen font-sans flex justify-center flex-col items-start max-w-7xl">
+    <slot />
+  </div>
+</main>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,74 @@
+<div class="login flex gap-16 max-w-4xl">
+	<h1 class="font-heading text-white text-10xl">Login</h1>
+	<form action="">
+		<p class="text-white mb-8 text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit ipsam eaque, illum incidunt quo deleniti non minima in ratione temporibus doloremque perferendis ducimus accusamus cumque suscipit architecto, tempora, facere eveniet.</p>
+	<div class="inline grid grid-cols-2 gap-5" action="POST">
+		<div class="flex-1">
+			<label for="name" class="block mb-2 text-sm font-medium text-slate-900 dark:text-slate-300"
+				>Name</label
+			>
+			<input
+				type="text"
+				id="name"
+				class="bg-slate-50 flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder="Doe"
+				required
+			/>
+		</div>
+		<div class="flex-1">
+			<label for="email" class="block mb-2 text-sm font-medium text-slate-900 dark:text-slate-300"
+				>Email</label
+			>
+			<input
+				type="email"
+				id="email"
+				class="bg-slate-50 flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder="doe@gmail.com"
+				required
+			/>
+		</div>
+		<div class="flex-1">
+			<label for="password" class="block mb-2 text-sm font-medium text-slate-900 dark:text-slate-300"
+				>Password</label
+			>
+			<input
+				type="password"
+				id="password"
+				class="bg-slate-50 flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder="password..."
+				required
+			/>
+		</div>
+		<div class="flex-1">
+			<label for="confirm_password" class="block mb-2 text-sm font-medium text-slate-900 dark:text-slate-300"
+				>Confirm Password</label
+			>
+			<input
+				type="password"
+				id="confirm_password"
+				class="bg-slate-50 flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder="password..."
+				required
+			/>
+		</div>
+		<div class="flex-1">
+			<label for="birthday" class="block mb-2 text-sm font-medium text-slate-900 dark:text-slate-300"
+				>Birthday</label
+			>
+			<input
+				type="birthday"
+				id="birthday"
+				class="bg-slate-50 flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder="8/10/1988"
+				required
+			/>
+		</div>
+		<button
+			type="submit"
+			class="bg-blue-50 text-2xl mt-5 w-full block grid-rows-5 leading-none tracking-wider h-42 self-end font-heading flex-1 border border-slate-300 text-white text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-blue-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+		>
+			Sign Up
+		</button>
+	</div>
+</form>
+</div>
